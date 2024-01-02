a3c6d747c4df986bdf3b2f47d77d60f9b0cf215c
Jesse <jesse007@ymail.com>
2024-01-02 21:00:45 +0000

    Include stdout and stderr to file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ set -eu
 
 export GITHUB="true"
 
-sh -c "/bin/drone-ssh $*"
+{
+    sh -c "/bin/drone-ssh $*"
+} 2> /tmp/errFile | tee /tmp/outFile
+
+stdout=$(cat /tmp/outFile)
+stderr=$(cat /tmp/errFile)
+echo "stdout=${stdout//$'\n'/\\n}" >> $GITHUB_OUTPUT
+echo "stderr=${stderr//$'\n'/\\n}" >> $GITHUB_OUTPUT

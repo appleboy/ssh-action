384d12c1a479148073699ce9036a746b01ff7f38
Jesse <jesse007@ymail.com>
2024-01-02 21:20:17 +0000

    remove stderr

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 set -eu
+set -o pipefail
 
 export GITHUB="true"
 
 {
     sh -c "/bin/drone-ssh $*"
-} 2> /tmp/errFile | tee /tmp/outFile
+} | tee /tmp/outFile
 
 stdout=$(cat /tmp/outFile)
-stderr=$(cat /tmp/errFile)
 echo "stdout=${stdout//$'\n'/\\n}" >> $GITHUB_OUTPUT
-echo "stderr=${stderr//$'\n'/\\n}" >> $GITHUB_OUTPUT
